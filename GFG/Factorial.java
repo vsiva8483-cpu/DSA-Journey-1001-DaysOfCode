6d83934ccaa031fde787f91f71b811b65301faa5
Venkateswarlu vankaraboina <vsiva8483@gmail.com>
2026-06-01 16:03:27 +0530

    Create Factorial.java

---
+++ GFG/Factorial.java
@@ -0,0 +1,12 @@
+lass Solution {
+    // Function to calculate factorial of a number.
+    int factorial(int n) {
+        int fact=1;
+        for(int i=1; i<=n; i++){
+            fact=fact*i;
+        }
+        return fact;
+        // code here
+        
+    }
+}
