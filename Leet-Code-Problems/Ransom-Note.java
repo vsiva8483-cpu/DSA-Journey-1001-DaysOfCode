c0ea9e74c51659c95fefe45f46e452a5e1f42ca5
Venkateswarlu vankaraboina <vsiva8483@gmail.com>
2026-06-01 16:22:40 +0530

    Add Ransom Note solution implementation

---
+++ Leet-Code-Problems/Ransom-Note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count=new int[26]; 
+        for(int i=0; i<magazine.length();i++){
+            count[magazine.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<ransomNote.length(); i++){
+            char ch=ransomNote.charAt(i);
+            if(count[ch-'a']==0){
+                return false;
+            }
+            count[ch-'a']--;
+        }
+        return true;
+    }
+}
