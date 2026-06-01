2093ca5fe343a231a8f00a428d05024db4bc4961
Venkateswarlu vankaraboina <vsiva8483@gmail.com>
2026-06-01 16:20:01 +0530

    Create Jump-Game.java

---
+++ Leet-Code-Problems/Jump-Game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxreach=0;
+        for(int i=0; i<nums.length;i++){
+            if(i>maxreach){
+                return false;
+            }
+            maxreach=Math.max(maxreach,i+nums[i]);
+        }
+        return true;
+    }
+}
+           
